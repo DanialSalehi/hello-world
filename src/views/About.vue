aa92c8dd2d75234e3f479c33289d507820172946
DanialSalehi <d.salehi.develop@gmail.com>
2021-01-21 13:05:18 +0330

    finish

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,11 @@
 <template>
   <div class="about">
     <v-layout column>
-      
-   
-    <h1>Hello Mr.Salehi</h1>
-    <br/>
-    <br/>
-    <h2>i used Vuetify for UI</h2>
-    <h3>i now that API_KEY </h3>
-     </v-layout>
+      <h1>Hello Mr.Salehi</h1>
+      <br />
+      <br />
+      <h2>i used Vuetify for UI</h2>
+      <h3>i now that API_KEY...</h3>
+    </v-layout>
   </div>
 </template>
