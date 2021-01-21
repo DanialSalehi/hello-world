8a573d2b9e6233b96e12156bff1be6980a8cad21
DanialSalehi <d.salehi.develop@gmail.com>
2021-01-21 11:44:02 +0330

    first up

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,30 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="home" id="imap">
+    <mapir :center="center" :apiKey="apiKey"></mapir>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+import {mapir} from 'mapir-vue';
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
-  }
+    mapir
+  },
+  data() {
+      return {
+        apiKey: process.env.API_KEY,
+        center: [51.420296, 35.732379]
+      };
+    }
 };
 </script>
+
+<style>
+  #imap {
+    width: 90vw;
+    height: 90vh;
+  }
+</style>
