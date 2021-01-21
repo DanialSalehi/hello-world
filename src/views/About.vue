906334bb58a305992fccaaa50038318a09fe6db0
DanialSalehi <d.salehi.develop@gmail.com>
2021-01-21 13:02:35 +0330

    13:02

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <v-layout column>
+      
+   
+    <h1>Hello Mr.Salehi</h1>
+    <br/>
+    <br/>
+    <h2>i used Vuetify for UI</h2>
+    <h3>i now that API_KEY </h3>
+     </v-layout>
   </div>
 </template>
